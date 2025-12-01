f89db090239874f6a9552cd0541acaaf52d4f079
alinadeem13 <ali.nadeem@planlabsolutions.com>
2025-12-01 16:27:21 +0500

    Learned About variables, datatypes, operators and other asics

--- Basics/DataTypes.py
+++ Basics/DataTypes.py
@@ -1,1 +1,49 @@
-print("Hello World")
+# print("Hello ")
+name = "Ali";
+# print("My name is " + name)
+price = 20
+# print( price)
+
+# print(type(price))
+
+# None -> Means no value has assigned to a variable
+# oneVariable = None
+# print(oneVariable)
+# isTrue = True;
+
+# print(type(isTrue))
+
+#  Type of Operators: +, -, *, /, %, //, **
+
+variable1 = 10
+variable2 = "90"
+# print(variable1 + variable2)
+# print(variable1 - variable2)
+# print(variable1 * variable2)
+# print(variable1 / variable2)  # float division
+# print(variable1 // variable2)  # integer division
+# print(variable1 % variable2)  # modulus operator
+# print(variable1 ** variable2)  # exponentiation operator
+
+# print(variable1 == variable2)   #FALSE
+
+# Logical Operators ============
+
+# print(not True)  # False
+
+# print(variable1 > 5 and variable1 < 15)  # True
+# print(variable1 and variable2)
+
+Stringa = '24';
+Stringb = '45';
+Stringc = 45
+Stringd = '90'
+# print(Stringa + Stringb)  # 2445
+# print(Stringc + int(Stringd))  # 2445
+
+#  =========== Taking Input type value,  note: alwyays input value is string type =============
+
+userInput = input("Enter your age: ")
+# print("Your age is ", userInput , "Typee: ", type(userInput))
+userInput2 = int(input("Enter your age: "))
+# print("Your age is ", userInput2 , "Typee: ", type(userInput2))
