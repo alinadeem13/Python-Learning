789aeb3cc7cc797a53e8ada3dbcb0f66a6bdee13
alinadeem13 <ali.nadeem@planlabsolutions.com>
2025-12-10 15:10:17 +0500

    Learn I/O

--- Basics/InputOutput.py
+++ Basics/InputOutput.py
@@ -9,6 +9,21 @@ f = open(r"d:/Learning/MyProjects/Python/Python-Learning/Basics/demo.txt", "a")
 # print(content)
 
 content = f.write("\nI see now")
-print(content)
+# print(content)
 
-f.close()
+
+# f.close()
+
+with open(r"d:/Learning/MyProjects/Python/Python-Learning/Basics/demo.txt", "r") as f:
+    content = f.read()
+    print(content)
+
+# The file is automatically closed after the with block
+# print(f.closed)
+
+# import os
+# os.remove(r"d:/Learning/MyProjects/Python/Python-Learning/Basics/demo.txt")
+
+# os.rename(r"d:/Learning/MyProjects/Python/Python-Learning/Basics/demo.txt", r"d:/Learning/MyProjects/Python/Python-Learning/Basics/demo_renamed.txt")
+
+# import shutil
