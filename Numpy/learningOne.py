2f40b82b233e1e20f372fc31465da7960aeb68a8
alinadeem13 <ali.nadeem@planlabsolutions.com>
2025-12-18 14:27:08 +0500

    Learn Numpy basc, solve practce questions

---
+++ Numpy/learningOne.py
@@ -0,0 +1,89 @@
+# listOne = [1,2,3,4,5, "al", True, ['a','b'], (1,3,4), {name:'ALi'}]
+# print(listOne)
+
+listHere = [1,2,3,4,5]
+
+import numpy as np
+
+# print(type(np))
+
+arr = np.array([10,20,30,40, 50])
+
+# print(arr);
+# print(type(arr))
+
+# for i in arr:
+#     print(i)
+
+# result = []
+
+# for i in listHere:
+#     result.append(i*2)
+
+# print(result)    
+
+# print(arr*2)     # No Loops wriiten/required. but loop happend internally in C(fast)
+
+# print(arr.shape)
+# print(arr.ndim)
+
+# Arr2 = [
+#     [1,2,3],
+#     [4,5,6]]
+# print(Arr2)
+
+matrix = np.array([
+    [1,2,3],
+    [4,5,6]
+])
+
+# print(matrix)
+
+# print(matrix[1,2])
+
+# print(len(matrix))
+
+# for i in matrix:
+#     for j in i:
+#         print(j)
+
+
+# print(matrix[:,1])
+# print(matrix[1,:])
+
+# print(matrix.mean())
+
+# Question # 1:
+
+# numArr = np.array([1,2,3,4,5,6,7,8,9,10])
+
+# print(numArr*3);
+
+# # Question # 2:
+
+# twoDarray = np.array([
+#     [1,2,3],
+#     [4,5,6]
+# ])
+
+# print(twoDarray.shape)
+# print(twoDarray.ndim)
+
+# Question # 3:
+
+# print(twoDarray[0,2])
+
+# # Question # 4:
+
+# print(numArr.sum())
+# print(numArr.mean())
+# print(numArr.max())
+
+# # Question # 5:
+
+# numList = [1,2,3,4,5]
+
+# numPyArray = np.array([numList])
+# result = numPyArray + 10;
+# print(result)
+
